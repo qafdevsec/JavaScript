94a78255daa1c872471820e0c456204fb62b7780
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-14 17:25:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,21 @@
-function getUserData(username) {
-    // Assume that the username is used in an XPath query
-    var query = "//user[@username='" + username + "']";
-    var result = executeXPathQuery(query);
+function decodeEscapeSequence(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+
+// Strip off the HTML preamble of AJAX Response (if any) and replace <br>s with real carriage returns
+function stripHTML(xmlResponse) {
+    // Crude: Rips out XML content we don't want to display in the browser
+    // Remove everything up through and including <p> plus carriage return after
+    var result = xmlResponse;
+    var pIndex = xmlResponse.indexOf('<p>');
+    
+    if (pIndex > 0) {
+        result = xmlResponse.substring(pIndex + 4, xmlResponse.length);
+    }
+    
+    result = result.replaceAll("<br>", "\n"); // Replace all <br>'s with carriage returns
+
     return result;
-}
-
-function executeXPathQuery(query) {
-    // Insecure XPath query execution
-    var xmlDoc = new DOMParser().parseFromString(xmlData, "text/xml");
-    var result = xmlDoc.evaluate(query, xmlDoc, null, XPathResult.ANY_TYPE, null);
-    var userData = result.iterateNext();
-    return userData;
-}
-
-// Assume that xmlData is loaded from an external source
-var xmlData = "<data><user username='admin'>Admin User</user><user username='guest'>Guest User</user></data>";
-
-// Get user data based on user input
-var userInput = prompt("Enter username:");
-var userData = getUserData(userInput);
-
-if (userData) {
-    alert("User found: " + userData.textContent);
-} else {
-    alert("User not found");
 }
