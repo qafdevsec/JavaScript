9091cdd69d1873d7343ee81845acaee4cd896521
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-14 17:28:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
-function decodeEscapeSequence(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-};
-
-// Strip off the HTML preamble of AJAX Response (if any) and replace <br>s with real carriage returns
-function stripHTML(xmlResponse) {
-    // Crude: Rips out XML content we don't want to display in the browser'
-    // Remove everything up through and including <p> plus carriage return after
-    var result = xmlResponse;
-    var pIndex = xmlResponse.indexOf('<p>');
-    if (pIndex > 0) {
-        result = xmlResponse.substring(pIndex + 4, xmlResponse.length);
-    }
-    result = result.replaceAll("<br>", "\n"); // Replace all <br>'s with carriage returns'
-
+// Function to fetch user data based on the username
+function getUserData(username) {
+    var query = "//users/user[username='" + username + "']";
+    // Execute the XPath query and retrieve user data
+    var result = executeXPathQuery(query);
     return result;
+}
+
+// Function to execute the XPath query
+function executeXPathQuery(query) {
+    // Assume this function is responsible for executing the XPath query
+    // and returning the result from the XML document
+    var xmlDoc = getXmlDocument(); // Function to get the XML document
+    var result = xmlDoc.evaluate(query, xmlDoc, null, XPathResult.ANY_TYPE, null);
+    return result.stringValue;
+}
+
+// Function to get the XML document
+function getXmlDocument() {
+    // Assume this function retrieves and returns the XML document
+    var xmlString = "<root><users><user><username>admin</username><password>admin123</password></user></users></root>";
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(xmlString, "text/xml");
+    return xmlDoc;
 }
