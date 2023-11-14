18828077fc6dcfede2a2636b7425c2f48a880e71
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-14 17:18:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,27 @@
-// Function to retrieve user data from XML using XPath
 function getUserData(username) {
-    // Construct XPath query with user input
-    var xpathQuery = "//user[@username='" + username + "']";
-
-    // Execute XPath query on the XML document
-    var result = executeXPathQuery(xpathQuery);
-
-    // Process and display the result
-    processResult(result);
-}
-
-// Function to execute XPath query on the XML document
-function executeXPathQuery(xpathQuery) {
-    // Assume xmlDoc is the XML document you are querying
-    // This is just a simplified example, in a real scenario, you need to load an XML document securely
-    var xmlDoc = new DOMParser().parseFromString(xmlString, "text/xml");
-
-    // Use XPath to query the XML document
-    var result = xmlDoc.evaluate(xpathQuery, xmlDoc, null, XPathResult.ANY_TYPE, null);
-
+    // Assume that the username is used in an XPath query
+    var query = "//user[@username='" + username + "']";
+    var result = executeXPathQuery(query);
     return result;
 }
 
-// Function to process and display the XPath query result
-function processResult(result) {
-    // Extract data from the XPath result
-    var data = result.stringValue;
-
-    // Display the data (for demonstration purposes)
-    console.log("User data: " + data);
+function executeXPathQuery(query) {
+    // Insecure XPath query execution
+    var xmlDoc = new DOMParser().parseFromString(xmlData, "text/xml");
+    var result = xmlDoc.evaluate(query, xmlDoc, null, XPathResult.ANY_TYPE, null);
+    var userData = result.iterateNext();
+    return userData;
 }
 
-// Example usage
-var userInput = "admin' or '1'='1' or '";
-getUserData(userInput);
+// Assume that xmlData is loaded from an external source
+var xmlData = "<data><user username='admin'>Admin User</user><user username='guest'>Guest User</user></data>";
+
+// Get user data based on user input
+var userInput = prompt("Enter username:");
+var userData = getUserData(userInput);
+
+if (userData) {
+    alert("User found: " + userData.textContent);
+} else {
+    alert("User not found");
+}
