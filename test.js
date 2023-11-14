937e6b260ded1ff07ccdae80247b9172c3518da3
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-14 17:26:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,20 +2,18 @@ function decodeEscapeSequence(html) {
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
     return txt.value;
-}
+};
 
 // Strip off the HTML preamble of AJAX Response (if any) and replace <br>s with real carriage returns
 function stripHTML(xmlResponse) {
-    // Crude: Rips out XML content we don't want to display in the browser
+    // Crude: Rips out XML content we don't want to display in the browser'
     // Remove everything up through and including <p> plus carriage return after
     var result = xmlResponse;
     var pIndex = xmlResponse.indexOf('<p>');
-    
     if (pIndex > 0) {
         result = xmlResponse.substring(pIndex + 4, xmlResponse.length);
     }
-    
-    result = result.replaceAll("<br>", "\n"); // Replace all <br>'s with carriage returns
+    result = result.replaceAll("<br>", "\n"); // Replace all <br>'s with carriage returns'
 
     return result;
 }
