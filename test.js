30eca6f98491d7d66e03a3e3ca6cc86dc327d78e
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-14 17:29:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,21 @@
-// Function to fetch user data based on the username
-function getUserData(username) {
-    var query = "//users/user[username='" + username + "']";
-    // Execute the XPath query and retrieve user data
-    var result = executeXPathQuery(query);
-    return result;
+import { execSync } from 'child_process';
+import { Request } from 'express';
+
+function executeCommand(cmd) {
+    execSync(cmd);
 }
 
-// Function to execute the XPath query
-function executeXPathQuery(query) {
-    // Assume this function is responsible for executing the XPath query
-    // and returning the result from the XML document
-    var xmlDoc = getXmlDocument(); // Function to get the XML document
-    var result = xmlDoc.evaluate(query, xmlDoc, null, XPathResult.ANY_TYPE, null);
-    return result.stringValue;
+function handleRequest(req) {
+    const cmd = req.query.cmd;
+    executeCommand(cmd);
 }
 
-// Function to get the XML document
-function getXmlDocument() {
-    // Assume this function retrieves and returns the XML document
-    var xmlString = "<root><users><user><username>admin</username><password>admin123</password></user></users></root>";
-    var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(xmlString, "text/xml");
-    return xmlDoc;
-}
+// Assuming you have a valid Express request object
+const sampleRequest = {
+    query: {
+        cmd: 'ls -l' // Replace with a safe command
+    }
+};
+
+// Call the function to handle the request
+handleRequest(sampleRequest);
